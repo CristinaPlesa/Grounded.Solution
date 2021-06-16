51271997787459c69529046d6c9bd2908b3c4576
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-16 14:38:14 -0700

    :card_file_box: update music links

---
+++ Grounded/Migrations/20210616213719_UpdateMusicLinks.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Grounded.Migrations
+{
+    public partial class UpdateMusicLinks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "ResourceId",
+                keyValue: 8,
+                column: "ResourceMusic",
+                value: "https://open.spotify.com/embed/track/2tgw0vmJDgckHwW2PdYTGo?si=80bd03eb43244f3b?theme=0");
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "ResourceId",
+                keyValue: 9,
+                column: "ResourceMusic",
+                value: "https://open.spotify.com/embed/track/7ckJgqAp6jAUSJ9Av2t9LY?si=9a48827d174f4587?theme=0");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "ResourceId",
+                keyValue: 8,
+                column: "ResourceMusic",
+                value: "Let’s Dance David Bowie: https://open.spotify.com/embed/track/2tgw0vmJDgckHwW2PdYTGo?si=80bd03eb43244f3b?theme=0");
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "ResourceId",
+                keyValue: 9,
+                column: "ResourceMusic",
+                value: "Lila Rodriguez Jr.: https://open.spotify.com/embed/track/7ckJgqAp6jAUSJ9Av2t9LY?si=9a48827d174f4587?theme=0");
+        }
+    }
+}
